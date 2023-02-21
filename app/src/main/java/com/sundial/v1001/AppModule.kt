80a4633719d14731c32b974ed539ad789fec751e
aBock4 <arbockhorst@gmail.com>
2023-02-21 15:21:39 -0500

    Added App Module

---
+++ app/src/main/java/com/sundial/v1001/AppModule.kt
@@ -0,0 +1,11 @@
+package com.sundial.v1001
+
+import com.sundial.v1001.service.ITwilightService
+import com.sundial.v1001.service.TwilightService
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val appModule = module {
+    viewModel { MainViewModel(get()) }
+    single <ITwilightService>{ TwilightService() }
+}
