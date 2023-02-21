257a1e7ff3c084cc24c5dc54499e7109aed204c7
aBock4 <arbockhorst@gmail.com>
2023-02-21 15:01:52 -0500

    Added Twilight data class

---
+++ app/src/main/java/com/sundial/v1001/dto/Twilight.kt
@@ -0,0 +1,8 @@
+package com.sundial.v1001.dto
+
+import java.util.Date
+import com.google.gson.annotations.SerializedName
+
+data class Twilight (@SerializedName("sunrise") var sunrise : Date, @SerializedName("sunset") var sunset : Date, ){
+
+}
