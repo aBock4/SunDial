6e3fa86b86615e9faf8c8786dafe05f86a29f97f
belmonjd1 <belmonjd@mail.uc.edu>
2023-02-27 23:57:43 -0500

    Connected MainViewModel to MainActivity; added dependency to build.gradle; specified default application name

--- app/src/main/java/com/sundial/v1001/MainActivity.kt
+++ app/src/main/java/com/sundial/v1001/MainActivity.kt
@@ -8,16 +8,23 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.sundial.v1001.ui.theme.SunDialTheme
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModel : MainViewModel by viewModel<MainViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            viewModel.fetchData()
+            val twilight by viewModel.twilight.observeAsState(initial = emptyList())
             SunDialTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
