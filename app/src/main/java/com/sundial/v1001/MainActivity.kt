024eafa8cce2ae62c8101c36019fff32423e240c
aBock4 <arbockhorst@gmail.com>
2023-03-12 12:05:15 -0400

    .

--- app/src/main/java/com/sundial/v1001/MainActivity.kt
+++ app/src/main/java/com/sundial/v1001/MainActivity.kt
@@ -32,8 +32,6 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            viewModel.fetchData()
-            val twilight by viewModel.twilight.observeAsState(initial = emptyList<Twilight>())
             SunDialTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background){
