6f045e0b1f4e8e83be20bea4c7f25d4aeac49be7
aBock4 <arbockhorst@gmail.com>
2023-03-12 12:25:03 -0400

    Fixed formatting

--- app/src/main/java/com/sundial/v1001/MainActivity.kt
+++ app/src/main/java/com/sundial/v1001/MainActivity.kt
@@ -26,7 +26,7 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : ComponentActivity() {
 
-    private val viewModel : MainViewModel by viewModel<MainViewModel>()
+    private val viewModel: MainViewModel by viewModel<MainViewModel>()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,107 +34,135 @@ class MainActivity : ComponentActivity() {
         setContent {
             SunDialTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background){
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
                     TwilightFacts("Android")
                 }
             }
         }
     }
-}
 
 
-@Composable
-fun TwilightFacts(name: String) {
-    var sunrise by remember { mutableStateOf("")}
-    var sunset by remember { mutableStateOf("")}
-    val lexendFontFamily = FontFamily(
-        Font(R.font.lexendregular, FontWeight.Normal),
-        Font(R.font.lexendbold, FontWeight.Bold),
-        Font(R.font.lexendblack, FontWeight.Black),
-        Font(R.font.lexendlight, FontWeight.Light),
-        Font(R.font.lexendmedium, FontWeight.Medium)
-    )
-    val context = LocalContext.current
-    Box(
-        modifier = Modifier.fillMaxSize()
-            .background(color = Champagne),
-        contentAlignment = Alignment.Center
-    ) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(16.dp)
+    @Composable
+    fun TwilightFacts(name: String) {
+        var sunrise by remember { mutableStateOf("") }
+        var sunset by remember { mutableStateOf("") }
+        val lexendFontFamily = FontFamily(
+            Font(R.font.lexendregular, FontWeight.Normal),
+            Font(R.font.lexendbold, FontWeight.Bold),
+            Font(R.font.lexendblack, FontWeight.Black),
+            Font(R.font.lexendlight, FontWeight.Light),
+            Font(R.font.lexendmedium, FontWeight.Medium)
+        )
+        val context = LocalContext.current
+        Box(
+            modifier = Modifier.fillMaxSize()
+                .background(color = Champagne),
+            contentAlignment = Alignment.Center
         ) {
-            OutlinedTextField(
-                value = sunrise,
-                onValueChange = { sunrise = it },
-                label = { Text(stringResource(R.string.sunrise), fontFamily = lexendFontFamily, fontWeight = FontWeight.Bold)}
-            )
-            OutlinedTextField(
-                value = sunset,
-                onValueChange = { sunset = it },
-                label = { Text(stringResource(R.string.sunset), fontFamily = lexendFontFamily, fontWeight = FontWeight.Bold)},
-
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                OutlinedTextField(
+                    value = sunrise,
+                    onValueChange = { sunrise = it },
+                    label = {
+                        Text(
+                            stringResource(R.string.sunrise),
+                            fontFamily = lexendFontFamily,
+                            fontWeight = FontWeight.Bold
+                        )
+                    }
                 )
-            Button(
-                onClick = {
-                    Toast.makeText(context, "$sunrise $sunset", Toast.LENGTH_LONG).show()
+                OutlinedTextField(
+                    value = sunset,
+                    onValueChange = { sunset = it },
+                    label = {
+                        Text(
+                            stringResource(R.string.sunset),
+                            fontFamily = lexendFontFamily,
+                            fontWeight = FontWeight.Bold
+                        )
+                    },
+
+                    )
+                Button(
+                    onClick = {
+                        Toast.makeText(context, "$sunrise $sunset", Toast.LENGTH_LONG).show()
+                    }
+                ) {
+                    Text(
+                        text = "Save",
+                        color = Color.White,
+                        fontFamily = lexendFontFamily,
+                        fontWeight = FontWeight.Medium
+                    )
                 }
-            ){
-                Text(text = "Save", color = Color.White, fontFamily = lexendFontFamily, fontWeight = FontWeight.Medium)
             }
         }
     }
-}
 
-@Composable
-fun SearchBar() {
-    var text by remember { mutableStateOf("") }
-    val lexendFontFamily = FontFamily(Font(R.font.lexendbold, FontWeight.Bold))
-    Surface(
-        elevation = 4.dp
-    ) {
-        Row(
-            modifier = Modifier
-                .background(color = Orange)
-                .fillMaxWidth()
-                .padding(16.dp)
-
+    @Composable
+    fun SearchBar() {
+        var text by remember { mutableStateOf("") }
+        val lexendFontFamily = FontFamily(Font(R.font.lexendbold, FontWeight.Bold))
+        Surface(
+            elevation = 4.dp
         ) {
-            OutlinedTextField(
-                value = text,
-                onValueChange = { text = it },
-                label = { Text(text = "Search Location", fontFamily = lexendFontFamily, fontWeight = FontWeight.Bold) },
+            Row(
                 modifier = Modifier
+                    .background(color = Orange)
                     .fillMaxWidth()
+                    .padding(16.dp)
 
-            )
+            ) {
+                OutlinedTextField(
+                    value = text,
+                    onValueChange = { text = it },
+                    label = {
+                        Text(
+                            text = "Search Location",
+                            fontFamily = lexendFontFamily,
+                            fontWeight = FontWeight.Bold
+                        )
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+
+                )
+            }
         }
     }
-}
 
 
-@Composable
-fun LogInButton() {
-    val lexendFontFamily = FontFamily(Font(R.font.lexendmedium, FontWeight.Medium))
-    Box(modifier = Modifier
-        .fillMaxSize()
-    ) {
-        Button(onClick = { /*TODO*/ },
+    @Composable
+    fun LogInButton() {
+        val lexendFontFamily = FontFamily(Font(R.font.lexendmedium, FontWeight.Medium))
+        Box(
             modifier = Modifier
-                .align(Alignment.BottomEnd)
-                .padding(16.dp)
+                .fillMaxSize()
         ) {
-            Text(text = "Log in", fontFamily = lexendFontFamily, fontWeight = FontWeight.Medium)
+            Button(
+                onClick = { /*TODO*/ },
+                modifier = Modifier
+                    .align(Alignment.BottomEnd)
+                    .padding(16.dp)
+            ) {
+                Text(text = "Log in", fontFamily = lexendFontFamily, fontWeight = FontWeight.Medium)
+            }
         }
     }
-}
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    SunDialTheme {
-        TwilightFacts("Android")
-        LogInButton()
-        SearchBar()
+    @Preview(showBackground = true)
+    @Composable
+    fun DefaultPreview() {
+        SunDialTheme {
+            TwilightFacts("Android")
+            LogInButton()
+            SearchBar()
+        }
     }
 }
