6812ac75f40ca725a56a60adfc842a09dbc139da
greenlem <65173994+greenlem@users.noreply.github.com>
2023-03-24 10:50:46 -0400

    Permission check and onInactive unsubscribe

--- app/src/main/java/com/sundial/v1001/LocationLiveData.kt
+++ app/src/main/java/com/sundial/v1001/LocationLiveData.kt
@@ -4,6 +4,7 @@ import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
 import android.location.Location
+import android.os.Looper
 import androidx.core.app.ActivityCompat
 import androidx.lifecycle.LiveData
 import com.google.android.gms.location.LocationCallback
@@ -44,7 +45,24 @@ class LocationLiveData(var context: Context) : LiveData<LocationDetails>() {
     }
 
     private fun startLocationUpdates() {
-        fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback)
+        if (ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper())
     }
 
     private fun setLocationData(location: Location?) {
@@ -56,6 +74,7 @@ class LocationLiveData(var context: Context) : LiveData<LocationDetails>() {
 
     override fun onInactive() {
         super.onInactive()
+        fusedLocationClient.removeLocationUpdates(locationCallback)
     }
 
     private val locationCallback = object : LocationCallback() {
