14f68deb420370059ab9c9a003b7eac4441fc13d
aBock4 <arbockhorst@gmail.com>
2023-04-04 13:04:11 -0400

    Added saveLocation() and saveUser() functions

--- app/src/main/java/com/sundial/v1001/MainViewModel.kt
+++ app/src/main/java/com/sundial/v1001/MainViewModel.kt
@@ -12,11 +12,14 @@ import com.sundial.v1001.service.ITwilightService
 import com.sundial.v1001.service.TwilightService
 import kotlinx.coroutines.launch
 import com.google.firebase.storage.FirebaseStorage
+import com.sundial.v1001.dto.LocationDetails
 import org.koin.core.component.getScopeId
 
 class MainViewModel (private var twilightService : ITwilightService = TwilightService()) : ViewModel() {
     var twilight : MutableLiveData<List<Twilight>> = MutableLiveData<List<Twilight>>()
     var user : User? = null
+    var location : LocationDetails ? =null
+    var locationName : String? =null
 
     private lateinit var firestore : FirebaseFirestore
 
@@ -31,25 +34,21 @@ class MainViewModel (private var twilightService : ITwilightService = TwilightSe
             twilight.postValue(innerTwilight)
         }
     }
-    fun saveLocation(longitude : String, latitude: String) {
-        user?.let { user ->
-            val document =
-                    firestore.collection("users").document(user.uid).collection("locations")
-                        .document()
-            var currentLongitude = longitude
-            var currentLatitude = latitude
-            currentLongitude = document.id
-            var handle = document.set(currentLongitude)
-            handle.addOnSuccessListener {
-                Log.d("Firebase", "Document Saved")
-            }
-            handle.addOnFailureListener { Log.e("Firebase", "Save failed $it") }
-
-            currentLatitude = document.id
-            handle = document.set(currentLatitude)
-            handle.addOnSuccessListener {
-                Log.d("Firebase", "Document Saved")
-            }
+    fun saveLocation() {
+        user?.let {
+                user ->
+               val document = firestore.collection("users").document(user.uid).collection("locations").document()
+                locationName = document.id
+                val handle = document.set(location!!)
+                handle.addOnSuccessListener { Log.d("Firebase", "Location Saved") }
+                handle.addOnFailureListener { Log.e("Firebase", "Save failed $it") }
+        }
+    }
+    fun saveUser() {
+        user?.let{
+                user ->
+            val handle = firestore.collection("users").document(user.uid).set(user)
+            handle.addOnSuccessListener { Log.d("Firebase", "Document Saved") }
             handle.addOnFailureListener { Log.e("Firebase", "Save failed $it") }
         }
     }
